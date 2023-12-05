7da88b3d02a6022e7e0e6237a0490bedb09d4c9e
Manel-Oulds <oulds.manel@gmail.com>
2023-12-05 01:48:26 -0500

    merging

--- src/scripts/australia.js
+++ src/scripts/australia.js
@@ -15,7 +15,7 @@ class Australia {
       
     let timer = document.getElementById("timer");
     
-    let div_timer = `<img style="width:50px; height:50px; " src="../../assets/timer.png"/> <p> ${this.seconds} </p>`;
+    let div_timer = `<img style="width:50px; height:50px; " src="assets/timer.png"/> <p> ${this.seconds} </p>`;
     // timer.innerHTML = `<img style="width:50px; height:50px; padding: 5px" src="../assets/timer.png"/> ${this.seconds}`; 
     timer.innerHTML  = div_timer;  
     let timerId = setInterval(() => {
