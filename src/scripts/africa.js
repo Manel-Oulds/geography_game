d919b2b894ed9ba240cb0d580c765e428aacd98c
Manel-Oulds <oulds.manel@gmail.com>
2023-12-05 01:57:38 -0500

    build success

--- src/scripts/africa.js
+++ src/scripts/africa.js
@@ -14,7 +14,7 @@ class Africa {
     this.seconds = 180;
     let timer = document.getElementById("timer");
 
-    let div_timer = `<img style="width:50px; height:50px; " src="../assets/timer.png"/> <p style="border: 2px solide white"> ${this.seconds} </p>`;
+    let div_timer = `<img style="width:50px; height:50px; " src="assets/timer.png"/> <p style="border: 2px solide white"> ${this.seconds} </p>`;
     // timer.innerHTML = `<img style="width:50px; height:50px; padding: 5px" src="../assets/timer.png"/> ${this.seconds}`;
     timer.innerHTML = div_timer;
     let timerId = setInterval(() => {
@@ -29,7 +29,7 @@ class Africa {
         });
       } else {
         this.seconds--;
-        let div_timer = `<img style="width:50px; height:50px; " src="../assets/timer.png"/> <p> ${this.seconds} </p>`;
+        let div_timer = `<img style="width:50px; height:50px; " src="assets/timer.png"/> <p> ${this.seconds} </p>`;
         // timer.innerHTML = `<img style="width:50px; height:50px; padding: 5px" src="../assets/timer.png"/> ${this.seconds}`;
         timer.innerHTML = div_timer;
       }
